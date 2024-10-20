3dfdf0dff527930b99b461cc7e005a7f0b75a886
Jeffrey Wong <70869230+jeffwong97@users.noreply.github.com>
2024-10-19 23:07:20 -0400

    Collect Static

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -101,9 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-STAIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -122,4 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
